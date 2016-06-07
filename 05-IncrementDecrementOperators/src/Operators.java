aa8ee65989bb609a29c7340d448b71d53516653f
MussabTanveer <mussab.tanveer9@gmail.com>
2016-06-07 15:12:00 +0500

    Use post and pre increment decrement operators

---
+++ 05-IncrementDecrementOperators/src/Operators.java
@@ -0,0 +1,26 @@
+
+public class Operators {
+
+	public static void main(String[] args) {
+		int num = 5;
+		
+		System.out.println(num++); // post increment
+		System.out.println(num);
+		System.out.println(++num); // pre increment
+		
+		System.out.println(num--); // post decrement
+		System.out.println(num);
+		System.out.println(--num); // pre decrement
+		
+		num += 5; // num = num + 5;
+		System.out.println(num);
+		
+		num *= 2; // num = num * 2;
+		System.out.println(num);
+		
+		num /= 5; // num = num / 5;
+		System.out.println(num);
+
+	}
+
+}

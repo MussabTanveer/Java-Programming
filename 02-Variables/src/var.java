6c2b8f5ce7980963c9c430efb80d963c633caaee
MussabTanveer <mussab.tanveer9@gmail.com>
2016-06-07 15:09:19 +0500

    Variable declaration and assignment in java

---
+++ 02-Variables/src/var.java
@@ -0,0 +1,26 @@
+
+public class var {
+
+	public static void main(String[] args) {
+		int num1 = 5;
+		long num2;
+		double num3 = 8.7;
+		double num4;
+		char ch = 'f';
+		num2 = 100000000;
+		num4 = 1000000.1845;
+		System.out.print("Number of apples: ");
+		System.out.println(num1);
+		System.out.print("Price of banglow: Rs.");
+		System.out.println(num2);
+		System.out.print("Litrres of water in jug: ");
+		System.out.println(num3);
+		System.out.print("Meters of running marathon: ");
+		System.out.println(num4);
+		System.out.print("English character: ");
+		System.out.println(ch);
+		
+
+	}
+
+}

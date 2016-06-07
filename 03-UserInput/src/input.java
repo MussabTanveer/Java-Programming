e6f0084177e266f3cc17214fc4b41d74020cb644
MussabTanveer <mussab.tanveer9@gmail.com>
2016-06-07 15:09:54 +0500

    Take input from user in java

---
+++ 03-UserInput/src/input.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class input {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter your name: ");
+		System.out.println(input.nextLine());
+		/*String name = input.nextLine();
+		System.out.println(name);*/
+
+	}
+
+}

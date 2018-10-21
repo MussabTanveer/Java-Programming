d26d2f30978ee00da566a309717605f743ab8d2a
MussabTanveer <mussab.tanveer9@gmail.com>
2018-10-22 01:11:24 +0500

    Arrays in java

---
+++ 23-Arrays/src/array.java
@@ -0,0 +1,18 @@
+
+public class array {
+
+	public static void main(String[] args) {
+		int arr[] = new int[10];
+		
+		arr[0] = 87;
+		arr[1] = 543;
+		arr[9] = 65;
+		
+		System.out.println(arr[1]);
+		
+		int arr2[] = {2,4,5,7,9};
+		
+		System.out.println(arr2[2]);
+	}
+
+}

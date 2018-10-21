227430c4e2a0d809d9b9b4940da7234de7ef4e49
MussabTanveer <mussab.tanveer9@gmail.com>
2018-10-22 01:16:43 +0500

    Enhanced for loop in java

---
+++ 27-EnhancedForLoop/src/forloop.java
@@ -0,0 +1,16 @@
+
+public class forloop {
+
+	public static void main(String[] args) {
+		int arr[] = {3,4,5,6,7};
+		int total = 0;
+		
+		for(int x: arr) {
+			total += x;
+		}
+		
+		System.out.println(total);
+
+	}
+
+}

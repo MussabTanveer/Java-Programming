a018b00979e74a1fbcbdf6763fd94f679a70fb6e
MussabTanveer <mussab.tanveer9@gmail.com>
2018-10-21 23:34:10 +0500

    For loop in java

---
+++ 18-ForLoop/src/forloop.java
@@ -0,0 +1,14 @@
+
+public class forloop {
+
+	public static void main(String[] args) {
+		for(int count = 1; count <= 10; count++) {
+			System.out.println(count);
+		}
+		
+		for(int count = 6; count <= 21; count+=3) {
+			System.out.println(count);
+		}
+	}
+
+}

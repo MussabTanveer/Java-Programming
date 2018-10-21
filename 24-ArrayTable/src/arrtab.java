1a7149c597e8009dbeace2341996faec5967c66e
MussabTanveer <mussab.tanveer9@gmail.com>
2018-10-22 01:11:43 +0500

    Array table in java

---
+++ 24-ArrayTable/src/arrtab.java
@@ -0,0 +1,13 @@
+
+public class arrtab {
+
+	public static void main(String[] args) {
+		System.out.println("Index\tValue");
+		int arr[] = {18,12,32,54,2};
+		
+		for (int counter = 0; counter < arr.length; counter++) {
+			System.out.println(counter + "\t" + arr[counter]);
+		}
+	}
+
+}
